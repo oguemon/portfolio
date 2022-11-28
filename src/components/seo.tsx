f08cf51636a924a25cd49c43b31be5006112436b
Ogura Katsuya <contact@oguemon.com>
2022-11-28 23:17:26 +0900

    Personのスキーマ情報を追加

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -19,5 +19,81 @@ export default function SEO({ title, description, ogpimg = "" }) {
     <meta property="og:description" content={description}/>
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:site" content="@oguemon_com"/>
+    <script type="application/ld+json" dangerouslySetInnerHTML={{
+      __html: `
+      {
+        "@context": "https://schema.org/",
+        "@type": "Person",
+        "name": "小倉 且也",
+        "url": "https://ogura.biz/",
+        "address": {
+          "@type": "PostalAddress",
+          "addressCountry": "JP",
+          "addressRegion": "東京都",
+          "addressLocality": "千代田区"
+        },
+        "alumniOf": {
+          "@type": "CollegeOrUniversity",
+          "name": "大阪大学",
+          "url": "https://www.osaka-u.ac.jp/",
+          "address": {
+            "@type": "PostalAddress",
+            "addressCountry": "JP",
+            "addressRegion": "大阪府",
+            "addressLocality": "吹田市",
+            "streetAddress": "山田丘1-1",
+            "postalCode": "565-0871"
+          }
+
+        },
+        "award": "第6回学生スマートフォンアプリコンテスト 最優秀賞（2018）",
+        "familyName": "小倉",
+        "givenName": "且也",
+        "jobTitle": "Web開発者, ブロガー",
+        "publishingPrinciples": [
+          {
+            "@type": "WebSite",
+            "name": "おぐえもん.com",
+            "url": "https://oguemon.com/",
+            "dateCreated": "2016-12-18"
+          },
+          {
+            "@type": "WebSite",
+            "name": "簡単！ザックリ手取り給料計算機",
+            "url": "https://oguemon.com/tax-calc/",
+            "dateCreated": "2018-12-07"
+          },
+          {
+            "@type": "Book",
+            "name": "大学1年生もバッチリ分かる線形代数入門",
+            "url": "http://www.pleiades-publishing.co.jp/genre/sugaku.html",
+            "author": "小倉 且也",
+            "bookFormat": "Paperback",
+            "dateCreated": "2021-06-18",
+            "isbn": "9784903814148",
+            "numberOfPages": "236",
+            "publisher": {
+              "@type": "Organization",
+              "name": "プレアデス出版",
+              "url": "http://www.pleiades-publishing.co.jp/"
+            }
+          }
+        ],
+        "worksFor": {
+          "@type": "Organization",
+          "name": "サイボウズ株式会社",
+          "url": "https://cybozu.co.jp/",
+          "address": {
+            "@type": "PostalAddress",
+            "addressCountry": "JP",
+            "addressRegion": "東京都",
+            "addressLocality": "中央区",
+            "streetAddress": "日本橋二丁目7-1 東京日本橋タワー27階",
+            "postalCode": "103-6027"
+          }
+        }
+      }
+      `
+    }} />
   </>
 }
