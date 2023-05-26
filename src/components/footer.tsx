5ae5cfe351448501db5d8db9415b399c707ae193
Ogura Katsuya <contact@oguemon.com>
2023-05-27 08:03:47 +0900

    フッターに現在の年を追加

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import "../styles/footer.scss";
 
 export default function Footer() {
+  const year = (new Date()).getFullYear();
 
   return <>
     <footer>
       <div className="wrapper">
-        <div className="copyright">Copyright © 2021 OGURA KATSUYA All Rights Reserved.</div>
+        <div className="copyright">Copyright © 2021-{year} OGURA KATSUYA All Rights Reserved.</div>
       </div>
     </footer>
   </>
